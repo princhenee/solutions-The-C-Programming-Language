3193f3ba3729a0c9d0e06f65ab3a1a18c4eac29a
princhenee <princhenee@gmail.com>
2015-02-01 21:57:50 +0800

    exercise 1-7

---
+++ chap_1/1-7/printf_EOF.c
@@ -0,0 +1,21 @@
+/*
+ * Filename:    printf_EOF.c
+ * Authro:      Ge CHEN <princhenee1991@126.com>
+ * Date:        2015-02-01
+ *
+ * exercise 1-7
+ *
+ * Write a program to print the value of EOF.
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int c = EOF ;
+    printf( "The value of 'EOF' is %d\n", c);
+
+    return 0;
+}
